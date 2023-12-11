d3d9ec7791bf1cb4c864012cffc14d63cf30a639
PiotrBa <piotr.balazyk@wp.pl>
2023-12-11 12:51:54 +0000

    Code formatting for security

--- src/main/java/com/piotrba/charity/security/SecurityConfig.java
+++ src/main/java/com/piotrba/charity/security/SecurityConfig.java
@@ -3,26 +3,38 @@ package com.piotrba.charity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    protected PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        return http.authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                .antMatchers("/").permitAll()
-                .antMatchers("form/donation-form").permitAll()
-                .and().formLogin();
-        return http.build();
+                .antMatchers("/homepage").permitAll()
+                .antMatchers("/form").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register/user").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .and()
+                .build();
     }
 }
