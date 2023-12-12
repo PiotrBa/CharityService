a37b2901a95bab1ccfe8de9214ea60ca4c7f6636
PiotrBa <piotr.balazyk@wp.pl>
2023-12-12 12:23:49 +0000

    Code formatting

--- src/main/java/com/piotrba/charity/viewController/DonationController.java
+++ src/main/java/com/piotrba/charity/viewController/DonationController.java
@@ -1,15 +1,20 @@
 package com.piotrba.charity.viewController;
 
 import com.piotrba.charity.entity.Donation;
+import com.piotrba.charity.entity.User;
+import com.piotrba.charity.repository.UserRepository;
 import com.piotrba.charity.service.CategoryService;
 import com.piotrba.charity.service.DonationService;
 import com.piotrba.charity.service.InstitutionService;
+import com.piotrba.charity.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/")
@@ -19,12 +24,14 @@ public class DonationController {
     private final CategoryService categoryService;
     private final InstitutionService institutionService;
     private final DonationService donationService;
+    private final UserRepository userRepository;
 
     @GetMapping("/form")
-    public String showForm(Model model){
+    public String showForm(Model model, Principal principal){
         model.addAttribute("categoryList", categoryService.findAllCategories());
         model.addAttribute("institutionList", institutionService.findAllInstitutions());
         model.addAttribute("donation", new Donation());
+        model.addAttribute("user", userRepository.getByUsername(principal.getName()));
         return "form/donation-form";
     }
 
